63d432f0b2a24ba2e34a89110739fae8bc699ab3
Y-mark7 <mark.yamaguti7@gmail.com>
2026-04-14 14:33:45 -0300

    Aula02

---
+++ studies/introducao/Aula02TiposPrimitivosPt01.java
@@ -0,0 +1,9 @@
+package studies.introducao;
+
+public class Aula02TiposPrimitivosPt01 {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int idade = 19;
+        System.out.println("A idade é: "+idade+" anos");
+    }
+}
