c2bce2d7cee8b6438b471be8d3255f3d56f85225
Y-mark7 <mark.yamaguti7@gmail.com>
2026-04-14 13:32:12 -0300

    Comentário

---
+++ Comentario.java
@@ -0,0 +1,15 @@
+public class Comentario {
+/**
+ * Este é um comentário de documentação para a classe Comentario.
+ * Ele pode ser usado para gerar documentação automática usando ferramentas como Javadoc.
+ */
+    public static void main(String[] args) {
+        // Isto é um comentário de linha única
+
+        /* Isto é um comentário
+        de multiplas
+        linha no código */
+
+        System.out.println("Comentários na linguagem Java");
+    }
+}
