cd5a9fb60534eeefe9dc921b2f5bac0928f5a4c2
Y-mark7 <mark.yamaguti7@gmail.com>
2026-04-16 09:59:22 -0300

    Aula05

---
+++ studies/introducao/Aula05String.java
@@ -0,0 +1,8 @@
+package studies.introducao;
+
+public class Aula05String {
+    public static void main(String[] args) {
+        String nome = "GU7";
+        System.out.println("Oi meu nome é "+nome);
+    }
+}
