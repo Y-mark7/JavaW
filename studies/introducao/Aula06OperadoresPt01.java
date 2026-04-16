267b98ec458739344877be3bc462cafba774dc3d
Y-mark7 <mark.yamaguti7@gmail.com>
2026-04-16 14:05:47 -0300

    Aula06

---
+++ studies/introducao/Aula06OperadoresPt01.java
@@ -0,0 +1,19 @@
+package studies.introducao;             //Aritméticos
+
+public class Aula06OperadoresPt01 {
+    public static void main(String[] args) {
+        //+, -, *, /
+        int numero01 = 10;
+        int numero02 = 20;
+
+        System.out.println("Resultado da soma: "+(numero01+numero02));
+        System.out.println("Resultado da subtração: "+(numero01-numero02));
+        System.out.println("Resultado da multiplicação: "+(numero01*numero02));
+        System.out.println("Resultado da divisão: "+(numero01/numero02));
+
+        double n1 = 10;
+        double n2 = 20;
+        double resultado = n1 / n2;
+        System.out.println("Resultado da divisão certa: "+resultado);
+    }
+}
