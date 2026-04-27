cc59c027bef9c54261b481642e94bb3dcb747317
Y-mark7 <mark.yamaguti7@gmail.com>
2026-04-27 11:19:11 -0300

    Aula14

---
+++ studies/introducao/Aula14EstruturasCondicionaisPt04.java
@@ -0,0 +1,47 @@
+package studies.introducao;             // Utilizando switch
+
+public class Aula14EstruturasCondicionaisPt04 {
+    public static void main(String[] args) {
+        // char, int, byte, short, enum, String
+        byte dia = 3;
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
+        char sx = 'M';
+        switch (sx) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Inválido");
+        }
+    }
+}
